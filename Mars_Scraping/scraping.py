82f78796e9d569895b7a662623e957131829587f
Sam Farber <sfarber@obtainmedrecs.com>
2022-10-31 11:06:51 -0400

    Through Module 10 Challenge

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -132,39 +132,6 @@ def scrape_hemisphere(html_text):
     }
     return hemispheres
 
-    # Quit the browser
-    browser.quit()
-
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
-
-
-
-    # def hemisphere_scrape(browser):
-#     # 1. Use browser to visit the URL 
-#     url = 'https://marshemispheres.com/'
-#     browser.visit(url)
-
-#     # 2. Create a list to hold the images and titles.
-#     hemisphere_image_urls = []
-
-#     # 3. Write code to retrieve the image urls and titles for each hemisphere.
-#     links = browser.find_by_css("a.product-item img")
-
-#     # Get URL from page
-#     for i in range(len(links)):
-#         hemispheres = {}
-#         browser.find_by_css("a.product-item img")[i].click()
-#         sample_element = browser.find_by_text("Sample").first
-#         hemispheres["url"] = sample_element["href"]
-
-#         # Get title from page
-#         title = browser.find_by_css("h2.title").text
-#         hemispheres["title"] = title
-
-#         hemisphere_image_urls.append(hemispheres)
-#         browser.back()
-
-#     # Return resulting dictionary
-#     return hemisphere_image_urls
